86661cd71cb416fc0f3896dd3882357dde417e1e
RiturajRoy <31593756+RiturajRoy@users.noreply.github.com>
2020-06-03 14:34:36 +0530

    Added the functionality to take class name from yml file

--- src/test/java/org/example/arc/Registered.java
+++ src/test/java/org/example/arc/Registered.java
@@ -1,3 +1,6 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.domain.JavaConstructorCall;
 import com.tngtech.archunit.core.importer.ImportOption;
@@ -9,23 +12,37 @@ import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.ConditionEvents;
 import com.tngtech.archunit.lang.SimpleConditionEvent;
 
+import java.io.File;
+import java.io.IOException;
+
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 @ArchTag(value = "Registered_Test")
 @AnalyzeClasses(packages="org.example",importOptions = {ImportOption.DoNotIncludeJars.class,
         ImportOption.DoNotIncludeTests.class,ImportOption.DoNotIncludeArchives.class})
 public class Registered {
-    public static String class_name = "ContextRequestFilter";
+
+        static ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        static JsonNode json;
+
+        static {
+            try {
+                json = mapper.readTree(new File("src\\test\\java\\headers_config.yml"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        private static String class_name=json.get("headers").get("classname").asText();
     static ArchCondition<JavaClass> mustBeRegisteredInTheApplicationClass =
-            new ArchCondition<JavaClass>("the given class must be registered in the application class") {
+            new ArchCondition<JavaClass>(" be registered in the application class") {
                 @Override
                 public void check(JavaClass item, ConditionEvents events) {
 
                     boolean check = false;
                     for (JavaConstructorCall javaConstructorCall : item.getConstructorCallsToSelf()) {
                         if (javaConstructorCall.getOrigin().getFullName().contains("Application.run")) {
-                                check = true;
-                                break;
+                            check = true;
+                            break;
 
                         }
                         if (check == true) {
@@ -40,6 +57,6 @@ public class Registered {
                 }
             };
     @ArchTest
-    static final ArchRule every_public_function_in_res = classes().that().haveSimpleNameContaining(class_name)
+    static final ArchRule filter_class_should_be_registered = classes().that().haveSimpleNameContaining(class_name)
             .should(mustBeRegisteredInTheApplicationClass);
 }
