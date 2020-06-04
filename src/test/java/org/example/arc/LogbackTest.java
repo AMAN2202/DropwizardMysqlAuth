a30a2e7e38f0ebd210715413b220139329dcb080
RiturajRoy <31593756+RiturajRoy@users.noreply.github.com>
2020-06-04 09:44:39 +0530

    Tests whether logback is being used for logging

--- src/test/java/org/example/arc/LogbackTest.java
+++ src/test/java/org/example/arc/LogbackTest.java
@@ -10,8 +10,10 @@ import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.ConditionEvents;
 import com.tngtech.archunit.lang.SimpleConditionEvent;
+import org.slf4j.impl.StaticLoggerBinder;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
 @ArchTag(value = "Logback_Test")
 @AnalyzeClasses(packages="org.example",importOptions = {ImportOption.DoNotIncludeJars.class,
         ImportOption.DoNotIncludeTests.class,ImportOption.DoNotIncludeArchives.class})
@@ -35,18 +37,27 @@ public class LogbackTest {
                     return check;
                 }
             };
-    static ArchCondition<JavaClass> all_Log_Methods_Should_Be_From_slf4j_or_logback =
+    static ArchCondition<JavaClass> all_Log_Methods_Should_Be_From_logback =
             new ArchCondition<JavaClass>("call slf4j logging methods") {
                 @Override
                 public void check(JavaClass item, ConditionEvents events) {
                     for (JavaMethod javaMethod : item.getMethods()) {
                         for (JavaMethodCall javaMethodCall : javaMethod.getMethodCallsFromSelf()) {
+                            boolean check=true;
                             if (javaMethodCall.getDescription().matches(".*Logger.*")) {
-                                if (!(javaMethodCall.getDescription().matches(".*org.slf4j.Logger.*")||javaMethodCall.getDescription().matches(".*ch.qos.logback.*"))) {
-                                    String message = String.format(
-                                            "Class %s is having a logging method call %s in method %s which is not from slf4j or logback package", item.getName(),javaMethodCall.getName(),javaMethod.getName());
-                                    events.add(SimpleConditionEvent.violated(item, message));
+                                if (javaMethodCall.getDescription().matches(".*org.slf4j.Logger.*")) {
+                                    final StaticLoggerBinder binder = StaticLoggerBinder.getSingleton();
+                                    if (!binder.getLoggerFactory().toString().matches(".*ch.qos.logback.*")) {
+                                        check = false;
+                                    }
+                                } else if (!javaMethodCall.getDescription().matches(".*ch.qos.logback.*")) {
+                                    check = false;
                                 }
+                            }
+                            if(!check){
+                                String message = String.format(
+                                        "Class %s is having a logging method call %s in method %s which is not from logback ", item.getName(),javaMethodCall.getName(),javaMethod.getName());
+                                events.add(SimpleConditionEvent.violated(item, message));
                             }
                         }
 
@@ -56,7 +67,8 @@ public class LogbackTest {
 
 
     @ArchTest
-    static final ArchRule slf4j_log_methods = classes().that(classesHavingLoggingMethods)
-            .should(all_Log_Methods_Should_Be_From_slf4j_or_logback);
+    static final ArchRule db_package_should_only_contain_dao_class=classes().that(classesHavingLoggingMethods)
+                                                                    .should(all_Log_Methods_Should_Be_From_logback);
+
 
 }
